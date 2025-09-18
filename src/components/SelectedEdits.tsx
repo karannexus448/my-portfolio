2531eaeb9a1a93f25bb322cce1f8a43cfe94a5ac
karannexus448 <kbist596@gmail.com>
2025-09-18 15:07:16 +0530

    Create SelectedEdits.tsx

---
+++ src/components/SelectedEdits.tsx
@@ -0,0 +1,12 @@
+<div className="edit-card">
+  <div style={{ padding: "56.25% 0 0 0", position: "relative" }}>
+    <iframe
+      src="https://player.vimeo.com/video/1117083402?badge=0&autopause=0"
+      frameBorder="0"
+      allow="autoplay; fullscreen; picture-in-picture; clipboard-write; encrypted-media; web-share"
+      referrerPolicy="strict-origin-when-cross-origin"
+      style={{ position: "absolute", top: 0, left: 0, width: "100%", height: "100%" }}
+      title="Cinematic Promo"
+    ></iframe>
+  </div>
+</div>
